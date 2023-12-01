d36ee432b426c12ab60ba4c378a4836d2a3db723
Amit <waghmare_a@fevcd>
2023-12-02 01:51:25 +0530

    navbar

--- scroll-background-change/src/App.js
+++ scroll-background-change/src/App.js
@@ -1,38 +1,41 @@
 import './App.css';
 import React, { useEffect, useRef } from "react";
-import {data} from "./data";
+import { data } from "./data";
+import Navbar from './Navbar';
 
 
 
 function App() {
 
   const GroupRef = useRef([]);
-  
-  const onScroll=(eL)=>{
+
+  const onScroll = (eL) => {
     const styles = GroupRef.current
-    .map((group,i)=>{
-      const rect = group.getBoundingClientRect()
+      .map((group, i) => {
+        const rect = group.getBoundingClientRect()
 
-      return{group, rect};
-    }).find((group) => group.rect.bottom >= window.innerHeight * 0.5);
+        return { group, rect };
+      }).find((group) => group.rect.bottom >= window.innerHeight * 0.5);
 
-    document.body.style.backgroundColor=`${styles.group.dataset.bgcolor}`;
+    document.body.style.backgroundColor = `${styles.group.dataset.bgcolor}`;
     document.body.style.color = styles.group.dataset.txtcolor;
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     window.addEventListener("scroll", onScroll)
   }, []);
 
   return (
     <>
       <div className='App'>
-        {data.map((group,i)=>(
-          <div 
-          ref={(eL) =>(GroupRef.current[i]=eL)} 
-          style={{height:'100vh'}}
-          data-bgcolor={group.theme.background}
-          data-textcolor={group.theme.text}
+        <Navbar />
+
+        {data.map((group, i) => (
+          <div
+            ref={(eL) => (GroupRef.current[i] = eL)}
+            style={{ height: '100vh' }}
+            data-bgcolor={group.theme.background}
+            data-txtcolor={group.theme.text}
           >
             <h1>{group.title}</h1>
             <p>{group.paragraph}</p>
--- scroll-background-change/src/data.js
+++ scroll-background-change/src/data.js
@@ -1,17 +1,27 @@
 export const data = [
     {
-        title: "Title 1",
-        paragraph: "Some words of whisdom.",
-        theme: {background: "yellow", text: "black"},
+        title: "Acta Non Verba",
+        paragraph: "Acta non verba is often translated as “actions, not words” but we found a different translation that we prefer: “deeds, not words.” The Latin word acta comes from the Latin root agere which evolved into actus, meaning “event” or “thing that was done,” which is the same root for the English “action.” However, acta literally means “register of events” and so we feel that “deeds” better conveys the gravity of our actions and makes us think more about the legacy we leave behind through the things we do. A sister phrase, actis non verbis, shares the same general meaning: “by means of things done, not by means of words.”",
+        theme: { background: "yellow", text: "black" },
     },
     {
-        title: "Title 2",
-        paragraph: "Yet Another paragraph about some philosophy.",
-        theme: {background: "lightblue", text: "grey"},
+        title: "Premeditatio Malorum",
+        paragraph: "",
+        theme: { background: "#ffb2ef", text: "white" },
     },
     {
-        title: "Title 3",
-        paragraph: "Some vague conclusion where its upto you to deside where to go.",
-        theme: {background: "black", text: "white"},
+        title: "Amor Fati",
+        paragraph: "",
+        theme: { background: "lightblue", text: "#a388ee" },
+    },
+    {
+        title: "Sympatheia",
+        paragraph: "",
+        theme: { background: "#ff69b4", text: "#90ee90" },
+    },
+    {
+        title: "Memento Mori",
+        paragraph: "Let us prepare our minds as if we’d come to the very end of life. Let us postpone nothing. Let us balance life’s books each day. . . .The one who puts the finishing touches on their life each day is never short of time.",
+        theme: { background: "black", text: "#ffffff" },
     },
 ];
